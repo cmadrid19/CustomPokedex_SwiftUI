64d0f1ae8f301b888f1fc42770b9b86d0e010b07
Max <maxim.macari.dam@gmail.com>
2020-11-09 03:37:41 +0100

    Initial Commit

---
+++ RegistrationApi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RegistrationApi
+//
+//  Created by Maxim Macari on 09/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RegistrationApi/RegistrationApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistrationApiApp.swift
+//  RegistrationApi
+//
+//  Created by Maxim Macari on 09/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct RegistrationApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
